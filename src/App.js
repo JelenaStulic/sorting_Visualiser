b117abc92bba7214b8f44ff0650de8498a383896
JelenaS <jelenastulic007@gmail.com>
2021-08-11 15:50:27 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SortingVisualiser from './SortingVisualizer/SortingVisualizer';
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SortingVisualiser></SortingVisualiser>
+    </div>
+  );
+}
+
+export default App;
